cb79905d001a925d3bdf22adfc2d7e68759ff151
Mahfujur Rahman <141043578+mahfujur221@users.noreply.github.com>
2024-05-22 21:22:33 +0600

    Add files via upload

---
+++ Basic Java/src/Object_Create/Person.java
@@ -0,0 +1,21 @@
+package Object_Create;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        Person p = new Person();     // Create Object person = " p "
+        p.name = "JIM";
+        p.age = 24;
+        p.display();             // Method Call
+    }
+
+    public void display() {    // Display method
+        System.out.println("My name :" + name);
+        System.out.println("My age :" + age);
+        System.out.println("My name is Sajjad Hossain Jim ");
+    }
+
+}
